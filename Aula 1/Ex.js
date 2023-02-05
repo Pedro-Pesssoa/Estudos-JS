cb073b7e01d46db7e882d79f71e26ff4e6367cfa
Pedro-Pesssoa <pedropessoacaeira@gmail.com>
2023-02-05 11:07:03 -0300

    exercicios aula 1

---
+++ Aula 1/Ex.js
@@ -0,0 +1,6 @@
+let nome = prompt("Digite seu nome");
+let sobrNome = prompt("Digite seu sobre nome");
+let idade = prompt("Digite sua idade");
+let id = Math.floor(Math.random() * 100);
+
+console.log(`Ola, ${nome}${sobrNome}, voce tem ${idade} e seu id é ${id}`);
---
+++ Aula 1/index.js
@@ -0,0 +1,28 @@
+//console.log escrve no console
+//maneiras de escrever uma string
+console.log("Hello, world !")
+console.log('Hello, world !')
+console.log(`Hello, world !`)
+
+//alert emite uma alerta com uma mensagem
+//alert("Alerta");
+
+//Variaveis
+
+    //Constante define um valor fixo
+    const constante = 0
+
+    //Let: 
+    let tipolet = 0
+    //Não pode ter outras com mesmo nome 
+    //Não pode ser acessada por outros blocos de código
+
+    //Var: 
+    var tipovar = 0;
+    //Pode ter outras com mesmo nome 
+    //Pode ser acessada por outros blocos de código
+
+let nome = prompt('Digite seu nome')
+
+console.log(`O usuario ${nome} eh diferenciado`)
+
